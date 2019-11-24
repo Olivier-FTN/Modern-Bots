323f2f17dfd3f3375487260523cdf72ac40c2dee
FONTAINE Olivier <54801256+Olivier-FTN@users.noreply.github.com>
2019-11-24 12:50:36 +0400

    Create view_menu.php

---
+++ view_menu.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="fr" class="no-js">
+<head>
+<?php
+    require_once('header.php');
+?>
+    <meta charset="utf-8">
+    <title>Modern Bots</title>
+    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="bootstrap/docs/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <link rel="stylesheet" href="annuaire.css">
+</head>
+  <body>
+    <?php include "menu.php";?>
+
+    <div class="container theme-showcase" role="main">
+<!--<div id="corps">-->
+    <div class="row">
+        <div class="col-md-6 col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="media block-div-image-accueil">
+                        <div class="media-body">
+                            <a href="view_elec.php"><div  class="badge badge-pill badge-primary badge-color">Electroménagers</div></a>
+                            <div class="type-article-accueil">Retrouvez ici, toutes les nouveautés électroménager qui viendront compléter avec goût et efficacité, l'aménagement de votre maison</div>
+                        </div>
+                        <div class="media-right">
+                            <a href="">
+                                <img class="media-object" src="images/electrom.png" alt="..." width="150px;">                                
+                            </a>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="media block-div-image-accueil">
+                        <div class="media-body">
+                          <a href=""> <div  class="badge badge-pill badge-primary badge-color">Drones</div></a>
+                            <div class="type-article-accueil">Découvrez ici l'étendue de notre gamme dédiée aux drones : drones jouets, drones DIY, drone de loisir, sous-marins, ailes volantes etc ... </div>
+                        </div>
+                        <div class="media-right">
+                            <a href="">
+                                <img class="media-object" src="images/drones.png" alt="..." width="200px;">
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="row">
+        <div class="col-md-6 col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="media block-div-image-accueil">
+                        <div class="media-body">
+                           <a href=""><div class="badge badge-pill badge-primary badge-color">Smartphones</div></a>
+                            <div class="type-article-accueil">Il vous faut de la puissance. Un e-mail de temps à autre .Vous pouvez retrouver notre sélection des meilleurs smartphones Android et iOS.</div>
+                        </div>
+                        <div class="media-right">
+                            <a href="">
+                                    <img class="media-object" src="images/smartphones.jpg" alt="..." height="125px;" width="205px;">
+                            </a>
+                            
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+
+                    <div class="media block-div-image-accueil">
+                        <div class="media-body">
+                           <a href="list_article.php?type_article=EQUIPEMENTS"> <div class="badge badge-pill badge-primary badge-color">Montres</div></a>
+                            <div class="type-article-accueil">Vous recherchez une montre connectée, vous êtes au bon endroit...</div>
+                        </div>
+                        <div class="media-right">
+                            <a href="list_article.php?type_article=EQUIPEMENTS">
+                                <img class="media-object" src="images/montres.png" alt="..." height="125px;" width="120px;">
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+</div>
+  </body>
+  <?php
+    require_once('footer.php');
+?>
+</html>
