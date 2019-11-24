02cb574611c089658643970c1aa96111e3a44307
FONTAINE Olivier <54801256+Olivier-FTN@users.noreply.github.com>
2019-11-24 12:51:00 +0400

    Create viewAccueil.php

---
+++ viewAccueil.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="fr" class="no-js">
+<head>
+<?php
+    require_once('header.php');
+?>
+    <meta charset="utf-8">
+    <title>Modern Bots</title> 
+    <link rel="stylesheet" href="annuaire.css">
+</head>
+
+<body>
+    <div id="contenue">
+        
+
+            <a class="bloc_lien" href = "view_inscription.php">
+                <h3>Inscription</h3>
+                <p>Vous devez vous inscrire pour accéder aux contenus du site.</p>
+            </a>
+        
+            <a class="bloc_lien" href = "#">
+                <h3>Connexion</h3>
+                <p>Déjà inscrit ? Connectez-vous pour accéder à votre session.</p>
+            </a>
+        
+    </div>
+</body>
+
+<?php
+    require_once('footer.php');
+?>
+</html>
