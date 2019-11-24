3eebb2c49afa2ed73d75b8e5e4ee5016c4e43bd1
FONTAINE Olivier <54801256+Olivier-FTN@users.noreply.github.com>
2019-11-24 12:48:48 +0400

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,26 @@
+<ul id="menu">
+        <li><a href="view_elec.php">Electroménagers</a>
+            <ul>
+                <li><a href="view_elec.php">Machines à laver</a></li>
+                <li><a href="#">Level 1.2</a></li>
+            </ul>
+        </li><li><a href="#">Drones</a>
+            <ul>
+                <li><a href="#">Level 2.1</a></li>
+                <li><a href="#">Level 2.2</a></li>
+
+
+            </ul>
+        </li><li><a href="#">Smartphones</a>
+            <ul>
+                <li><a href="#">Level 3.1</a></li>
+                <li><a href="#">Level 3.2</a></li>
+
+            </ul>
+        </li><li><a href="#">Montres</a>
+            <ul>
+                <li><a href="#">Level 1.1</a></li>
+                <li><a href="#">Level 1.2</a></li>
+            </ul>
+        </li><li><a href="index.php?action=Deconnexion">Déconnexion</a></li>
+    </ul>
