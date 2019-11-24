cdc6693a65b35547e98f97cd31513802d598c5d8
FONTAINE Olivier <54801256+Olivier-FTN@users.noreply.github.com>
2019-11-24 09:15:19 +0400

    Create modele.php

---
+++ modele.php
@@ -0,0 +1,25 @@
+<?php
+// Connexion à la BASE DE DONNÉES
+class DB {
+  private $cnx = null;
+  private $resultat = null;
+
+  function __construct(){
+
+    $this->cnx = new PDO("mysql:host=localhost;dbname=mb;charset=utf8", "root", "olivier" );
+}
+
+  function __destruct(){
+    if ($this->resultat!==null) { $this->resultat = null; }
+    if ($this->cnx!==null) { $this->cnx = null; }
+  }
+
+  // Exécution de la requête préparée
+  // La fonction peut être appelée plusieurs fois.
+  function Requete($strSQL, $tblValeur){
+    $this->resultat = $this->cnx->prepare($strSQL);
+    $this->resultat->execute($tblValeur);
+    return $this->resultat->fetchAll();
+  }
+}
+?>
