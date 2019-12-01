e5f1cebabf014bf36ed532ed2922e788b80e1107
FONTAINE Olivier <54801256+Olivier-FTN@users.noreply.github.com>
2019-12-01 12:49:31 +0400

    Update view_connexion.php

--- view_connexion.php
+++ view_connexion.php
@@ -9,18 +9,26 @@
     <link rel="stylesheet" href="annuaire.css">
 </head>
   <body>
-    <div class="form-cnx">
+    <div class="form-cnx2">
         <article>
-            <h1>Formulaire de connexion</h1>
+            <h1>Identifiez-vous</h1>
                 <form action="index.php?action=connexion" method="post">
+                    <label>Email :</label>
                     <input type="email" name="mail" id="mail" placeholder="Email"><br><br>
+                    <label>Mot de passe :</label>
                     <input type="password" name="mdp" id="mdp" placeholder="Mot de passe"><br><br>
-                    
-                    
+                                      
                     <input type="submit" name="action" value="connexion">
+                    
                 </form>
         </article>
+</br></br>
+        <div style="color:white;">Nouveau chez Modern Bots ?</div>
+      <a href = "view_inscription.php">Créer votre compte</a>
     </div>
+                
+        
+    
   </body>
   <?php
     require_once('footer.php');
