ff347c7eb1f243ef627696dbe41148b764073238
FONTAINE Olivier <54801256+Olivier-FTN@users.noreply.github.com>
2019-12-01 12:46:48 +0400

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,26 +1,34 @@
 <ul id="menu">
-        <li><a href="view_elec.php">Electroménagers</a>
-            <ul>
-                <li><a href="view_elec.php">Machines à laver</a></li>
-                <li><a href="#">Level 1.2</a></li>
-            </ul>
-        </li><li><a href="#">Drones</a>
-            <ul>
+        <li><a href="view_prod.php?cat=electromenager">Electroménagers</a>
+            <!--<ul>
+                <li><a href="view_prod.php?cat=electromenager">Machines à laver</a></li>
+                <li><a href="#">Réfrigérateurs</a></li>
+            </ul>-->
+        </li><li><a href="view_prod.php?cat=drone">Drones</a>
+            <!--<ul>
                 <li><a href="#">Level 2.1</a></li>
                 <li><a href="#">Level 2.2</a></li>
 
 
-            </ul>
-        </li><li><a href="#">Smartphones</a>
-            <ul>
+            </ul>-->
+        </li><li><a href="view_prod.php?cat=telephone">Smartphones</a>
+            <!--<ul>
                 <li><a href="#">Level 3.1</a></li>
                 <li><a href="#">Level 3.2</a></li>
 
-            </ul>
-        </li><li><a href="#">Montres</a>
-            <ul>
+            </ul>-->
+        </li><li><a href="view_prod.php?cat=montre">Montres</a>
+            <!--<ul>
                 <li><a href="#">Level 1.1</a></li>
                 <li><a href="#">Level 1.2</a></li>
-            </ul>
+            </ul>-->
         </li><li><a href="index.php?action=Deconnexion">Déconnexion</a></li>
-    </ul>
+
+        <!-- Barre de recherche -->
+        <li>
+            <form action="/search" id="searchthis" method="POST">
+                <input id="search" name="q" type="text" placeholder="Entrez votre recherche" />
+                <input id="search-btn" type="submit" value="Rechercher" />
+            </form>
+        </li>
+</ul>
