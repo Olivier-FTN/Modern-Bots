88a7e8500dedef780bfd71276d0890cbf6f91bbf
FONTAINE Olivier <54801256+Olivier-FTN@users.noreply.github.com>
2019-12-01 12:45:11 +0400

    Update header.php

--- header.php
+++ header.php
@@ -3,6 +3,15 @@
 <body>
     <div id="header">
     <img src="images/mb2.png"/>
-    </div>
+        <div class = "session_cli">Bonjour,
+        <?php
+        if(isset($_SESSION["userId"])){
+            echo $_SESSION["userNom"];
+        }else{   
+        echo '<a href = "view_connexion.php">Identifiez-vous</a>';
+        }
+        echo '</div>';
+    echo '</div>';
+    ?>
 </body>
 </html>
